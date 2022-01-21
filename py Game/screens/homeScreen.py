77ee89ab3ed09f0e3478a72e374bae1c5115d7b8
Eya Arfa <arfaaya0@gmail.com>
2022-01-22 00:03:03 +0100

    img

--- py Game/screens/homeScreen.py
+++ py Game/screens/homeScreen.py
@@ -8,9 +8,9 @@ pygame.display.set_caption("Button!")
 main_font = pygame.font.SysFont("cambria", 50)
 
 fen=window()
-background=pygame.image.load(r'F:\oussema\study\ingenierie\1er\pyGame\MyPyGame\py Game\images\Sans titre.png')
+background=pygame.image.load(r'py Game\images\bulls-and-cows-android.jpg')
 running=True
-button_surface = pygame.image.load(r'F:\oussema\study\ingenierie\1er\pyGame\MyPyGame\py Game\images\button.png')
+button_surface = pygame.image.load(r'py Game\images\button.png')
 button_surface = pygame.transform.scale(button_surface, (100, 50))
 button1 = Button(button_surface, 20, 20, "Button",screen,main_font)
 while running:
@@ -19,9 +19,9 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-        #button1.click(event)
-    #button1.show()
-    #button1.changeColor(pygame.mouse.get_pos())
+        # button1.click(event)
+    # button1.show()
+    # button1.changeColor(pygame.mouse.get_pos())
     pygame.display.update()
     pygame.display.flip()
 pygame.quit()
