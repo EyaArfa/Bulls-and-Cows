1e74bd389accd44ea2570edbc9832b12080eb0f8
Eya Arfa <arfaaya0@gmail.com>
2022-01-22 00:03:28 +0100

    img

--- py Game/screens/window.py
+++ py Game/screens/window.py
@@ -4,7 +4,7 @@ class window:
     def __init__(self) :
         self.screen=pygame.display.set_mode((500,500))
         pygame.display.set_caption('vache&taureau')
-        Icon = pygame.image.load(r'F:\oussema\study\ingenierie\1er\pyGame\MyPyGame\py Game\images\icon.png')
+        Icon = pygame.image.load(r'py Game\images\icon.png')
         pygame.display.set_icon(Icon)
     
         
