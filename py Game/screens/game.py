6099149914ae07fda380b2f4343480c69708e540
Eya Arfa <arfaaya0@gmail.com>
2022-01-22 00:49:04 +0100

    button1

--- py Game/screens/game.py
+++ py Game/screens/game.py
@@ -9,45 +9,46 @@ from random  import randint
 from window import window
 import pygame
 import sys
-fen=window()
-background=pygame.image.load(r'py Game\images\Sans titre.png')
-base_font = pygame.font.Font(None, 32)
-user_text = ''
-input_rect = pygame.Rect(200, 200, 140, 32)
 
-while True:
-    fen.screen.fill((0,0,0))
-    fen.screen.blit(background,(0,0))
-    for event  in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            # Check for backspace
-            if event.key == pygame.K_BACKSPACE:
-                # get text input from 0 to -1 i.e. end.
-                user_text = user_text[:-1]
-            # Unicode standard is used for string
-            # formation
-            else:
-                user_text += event.unicode    
-    pygame.display.update()
-    # draw rectangle and argument passed which should
-    # be on screen
-    pygame.draw.rect(fen.screen,pygame.Color('black') ,input_rect)
+def play():
+    fen=window()
+    background=pygame.image.load(r'py Game\images\Sans titre.png')
+    base_font = pygame.font.Font(None, 32)
+    user_text = ''
+    input_rect = pygame.Rect(200, 200, 140, 32)
+    while True:
+        fen.screen.fill((0,0,0))
+        fen.screen.blit(background,(0,0))
+        for event  in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                # Check for backspace
+                if event.key == pygame.K_BACKSPACE:
+                    # get text input from 0 to -1 i.e. end.
+                    user_text = user_text[:-1]
+                # Unicode standard is used for string
+                # formation
+                else:
+                    user_text += event.unicode    
+        pygame.display.update()
+        # draw rectangle and argument passed which should
+        # be on screen
+        pygame.draw.rect(fen.screen,pygame.Color('black') ,input_rect)
 
-    text_surface = base_font.render(user_text, True, (255, 255, 255))
-        
-    # render at position stated in arguments
-    fen.screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
-        
-    # set width of textfield so that text cannot get
-    # outside of user's text input
-    input_rect.w = max(100, text_surface.get_width()+10)
-        
-    # display.flip() will update only a portion of the
-    # screen to updated, not full area
-    pygame.display.flip()
+        text_surface = base_font.render(user_text, True, (255, 255, 255))
+            
+        # render at position stated in arguments
+        fen.screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+            
+        # set width of textfield so that text cannot get
+        # outside of user's text input
+        input_rect.w = max(100, text_surface.get_width()+10)
+            
+        # display.flip() will update only a portion of the
+        # screen to updated, not full area
+        pygame.display.flip()
 
 def count(ch,x):
     if(ch==x):
