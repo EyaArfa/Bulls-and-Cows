eecf4d0bb5fcaefd0715acd3c6710ef2b23547f5
OussemaZaier <oussamaweldlos@gmail.com>
2022-01-22 14:24:57 +0100

    main

--- py Game/screens/homeScreen.py
+++ py Game/screens/homeScreen.py
@@ -1,33 +1,47 @@
 import pygame
 from button import button
 from window import window
+import ctypes
+import os
+
+user32 = ctypes.windll.user32
+width1, height1 = user32.GetSystemMetrics(78), user32.GetSystemMetrics(79)
 pygame.init() #initialize the pygame library
+width=(width1//3)*2
+height=(height1//3)*2
+x=(width1//2)-(width//2)
+y=height1-height
+screen = pygame.display.set_mode((width, height))
+os.environ['SDL_VIDEO_WINDOW_POS'] = '%i,%i' % (x,y)
+os.environ['SDL_VIDEO_CENTERED'] = '0'
 
-screen = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Button!")
-main_font = pygame.font.SysFont("cambria", 50)
+pygame.display.flip()
+background=pygame.image.load(r'py Game\images\bg.png')
+background = pygame.transform.scale(background, (width, height))
 
-fen=window()
-background=pygame.image.load(r'py Game\images\bulls-and-cows-android.jpg')
 running=True
-bt=pygame.image.load(r'py Game\images\button.png')
-bt=pygame.transform.scale(bt, (400, 150))
-play= pygame.image.load('py Game\images\start.png')
-button1=button((220,220),30,play,fen.screen)
-# button2=button((220,260),30,bt,fen.screen)
-# button3=button((220,300),30,bt,fen.screen)
+
+play1= pygame.image.load('py Game\images\start.png')
+button1=button((width//2,height-(height//5)-120),30,play1,screen)
+
+play2= pygame.image.load('py Game\images\HS.png')
+button2=button((width//2,height-(height//5)-20),30,play2,screen)
+
+play3= pygame.image.load('py Game\images\HS.png')
+button3=button((width//2,height-(height//5)+80),30,play3,screen)
+
 while running:
-    fen.screen.fill((0,0,0))
-    fen.screen.blit(background,(0,0))
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
     button1.draw()
-    # button2.draw()
-    # button3.draw()
+    button2.draw()
+    button3.draw()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
         button1.click(event)
-        # button2.click(event)
-        # button3.clickExit(event)
+        button2.click(event)
+        button3.clickExit(event)
     # button1.show()
     # button2.show()
     # button3.show()
